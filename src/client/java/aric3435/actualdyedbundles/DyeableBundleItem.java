33b750b54cf508f1b2ccd343939905bda925acfb
aric3435 <157071384+arichornloverALT@users.noreply.github.com>
2024-09-20 17:06:47 -0500

    Create DyeableBundleItem.java

---
+++ src/client/java/aric3435/actualdyedbundles/DyeableBundleItem.java
@@ -0,0 +1,32 @@
+package aric3435.actualdyedbundles;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
+
+public class DyeableBundleItem extends Item {
+    public DyeableBundleItem(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, net.minecraft.entity.player.PlayerEntity player, Hand hand) {
+        ItemStack itemStack = player.getStackInHand(hand);
+        // WORK IN PROGRESS - aric3435
+        return new TypedActionResult<>(ActionResult.SUCCESS, itemStack);
+    }
+
+    public static void applyDye(ItemStack stack, int color) {
+        NbtCompound nbt = stack.getOrCreateNbt();
+        nbt.putInt("CustomColor", color);
+    }
+
+    public static int getColor(ItemStack stack) {
+        NbtCompound nbt = stack.getNbt();
+        return nbt != null && nbt.contains("CustomColor") ? nbt.getInt("CustomColor") : 0xFFFFFF; // Unidenified Colors - Aric3435
+    }
+}
