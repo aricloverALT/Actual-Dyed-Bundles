dd779580c09f10a5206e18bbcd6a11a1b81c936f
aric3435 <157071384+arichornloverALT@users.noreply.github.com>
2024-09-20 19:05:52 -0500

    Create DyeableBundleItemRendererMixin.java

---
+++ src/client/java/aric3435/actualdyedbundles/mixin/client/DyeableBundleItemRendererMixin.java
@@ -0,0 +1,26 @@
+package aric3435.actualdyedbundles.mixin;
+
+import aric3435.actualdyedbundles.DyeableBundleItem;
+import net.minecraft.client.render.item.ItemRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.model.BakedModel;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ItemRenderer.class)
+public class DyeableBundleItemRendererMixin {
+    private static final Identifier BUNDLE_TEXTURE = new Identifier("minecraft", "textures/item/bundle.png");
+
+    @Inject(method = "renderItem", at = @At("HEAD"), cancellable = true)
+    private void renderDyeableBundle(ItemStack stack, ModelTransformation.Mode mode, boolean leftHanded, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay, BakedModel model, CallbackInfo ci) {
+        if (stack.getItem() instanceof DyeableBundleItem) {
+            int color = DyeableBundleItem.getColor(stack);
+            // Apply color overlay to the item rendering - aric3435
+        }
+    }
+}
